ea4dfd622f904d505ad9895d0233cbba9a0501fb
Martin Ortbauer <mortbauer@gmail.com>
2016-06-04 14:54:43 +0200

    using promise to decide reconnect

--- static/src/middleware/websocket.js
+++ static/src/middleware/websocket.js
@@ -5,14 +5,11 @@ export default class WSClient{
         this.reconnectInterval = reconnectInterval;
         this.reconnectDecay = reconnectDecay;
         this.reconnectAttempts = 0;
-        this.websocket;
-        this.connecting = false;
         this.connect();
     };
     connect(){
-        if (!this.connecting){
+        var promise = new Promise((resolve,reject) => {
             console.log(`connecting to: ${this.url}`);
-            this.connecting = true;
             this.websocket = new WebSocket(this.url);
             this.websocket.onmessage = (event) => {
                 console.log('a websocket got ',event.data);
@@ -28,21 +25,25 @@ export default class WSClient{
                 }
             }
             this.websocket.onerror = (event) => {
-                this.connecting = false;
                 console.log('a websocket error happened');
-                this.reconnect();
+                reject(Error('a websocket error happened'));
             }
             this.websocket.onopen = (event) => {
                 this.reconnectAttempts = 0;
                 console.log('the websocket is ready');
             }
             this.websocket.onclose = (event) => {
-                this.connecting = false;
                 console.log('the websocket closed');
-                this.reconnect();
+                reject(Error('a websocket error happened'));
             }
-        };
-    };
+        });
+        promise.then(()=>{
+            console.log('the websocket is listening');
+        },()=>{
+            console.log('the websocket needs a reconnect');
+            this.reconnect()
+        });
+    }
 
     reconnect(){
         this.reconnectAttempts ++;
