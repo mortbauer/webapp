2347d08882fe4af8f7565a0a1113436a63b1b916
Martin Ortbauer <mortbauer@gmail.com>
2016-06-04 15:19:07 +0200

    promises are not needed, didn't actually work

--- static/src/middleware/websocket.js
+++ static/src/middleware/websocket.js
@@ -6,50 +6,50 @@ export default class WSClient{
         this.reconnectDecay = reconnectDecay;
         this.reconnectAttempts = 0;
         this.connect();
+        this.connected = false;
     };
     connect(){
-        var promise = new Promise((resolve,reject) => {
-            console.log(`connecting to: ${this.url}`);
-            this.websocket = new WebSocket(this.url);
-            this.websocket.onmessage = (event) => {
-                console.log('a websocket got ',event.data);
-                try{
-                    var msg = JSON.parse(event.data);
-                    if (!!msg.action){
-                        console.log('got message wich should be dispatched here');
-                    } else {
-                        console.log('got message without action');
-                    }
-                }catch(err){
-                    console.log('could not parse to json');
-                }
-            }
-            this.websocket.onerror = (event) => {
-                console.log('a websocket error happened');
-                reject(Error('a websocket error happened'));
-            }
-            this.websocket.onopen = (event) => {
-                this.reconnectAttempts = 0;
-                console.log('the websocket is ready');
-            }
-            this.websocket.onclose = (event) => {
-                console.log('the websocket closed');
-                reject(Error('a websocket error happened'));
-            }
-        });
-        promise.then(()=>{
-            console.log('the websocket is listening');
-        },()=>{
-            console.log('the websocket needs a reconnect');
+        console.log(`connecting to: ${this.url}`);
+
+        this.websocket = new WebSocket(this.url);
+
+        this.websocket.onmessage = (event) => {
+            this.handleIncomming(event.data);
+        }
+
+        this.websocket.onerror = (event) => {
+            console.log('websocket error');
+        }
+
+        this.websocket.onopen = (event) => {
+            console.log('websocket open');
+            this.reconnectAttempts = 0;
+        }
+
+        this.websocket.onclose = (event) => {
+            console.log('websocket closed');
             this.reconnect()
-        });
+        }
+    }
+
+    handleIncomming(data){
+        try{
+            var msg = JSON.parse(data);
+            if (!!msg.action){
+                console.log('got message wich should be dispatched here');
+            } else {
+                console.log('got message without action');
+            }
+        }catch(err){
+            console.log('could not parse to json');
+        }
     }
 
     reconnect(){
-        this.reconnectAttempts ++;
         var timeout = this.reconnectInterval * Math.pow(this.reconnectDecay, this.reconnectAttempts);
         console.log(`try to reconnect in: ${timeout}`);
         setTimeout(()=>this.connect(),timeout);
+        this.reconnectAttempts ++;
     };
 
     middleware(store) {
