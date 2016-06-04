2ba851fc02c5f9c6df8be04cf0a02ae6730fb677
Martin Ortbauer <mortbauer@gmail.com>
2016-06-04 06:51:05 +0200

    actually use productionconfig if PRODUCTION defined

--- application/__init__.py
+++ application/__init__.py
@@ -6,10 +6,12 @@ from flask.ext.bcrypt import Bcrypt
 from flask_sockets import Sockets
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
+
 if os.environ.get('PRODUCTION'):
-    app.config.from_object('config.BaseConfig')
+    app.config.from_object('config.ProductionConfig')
 else:
     app.config.from_object('config.TestingConfig')
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 sockets = Sockets(app)
