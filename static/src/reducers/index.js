2ad9b0530c94268e692284c44306a8eda56dd552
Martin Ortbauer <mortbauer@gmail.com>
2016-06-04 06:59:45 +0200

    add a lastAction to the state

--- static/src/reducers/index.js
+++ static/src/reducers/index.js
@@ -4,12 +4,17 @@ import {data} from './data'
 import {transactions} from './transactions'
 import { routerReducer } from 'react-router-redux'
 
+function lastAction(state = null, action) {
+  return action;
+}
+
 const rootReducer = combineReducers({
     routing: routerReducer,
     /* your reducers */
     auth,
     data,
     transactions,
+    lastAction,
 });
 
 export default rootReducer;
