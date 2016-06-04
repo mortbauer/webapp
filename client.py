6eb6822acf99cb7be55a5ae6fc5a350e35c1e8e1
Martin Ortbauer <mortbauer@gmail.com>
2016-06-04 06:51:37 +0200

    update to new test password

--- client.py
+++ client.py
@@ -1,10 +1,10 @@
 import requests
 
 BASE_URL = 'http://localhost:5000/api'
-r = requests.post(BASE_URL+'/get_token',json={'email':'mortbaur@gmail.it','password':'leopold'})
-if r.status == 200:
+r = requests.post(BASE_URL+'/get_token',json={'email':'mortbauer@gmail.com','password':'leopold'})
+if r.status_code == 200:
     t =r.json()['token']
-
     headers = {'Authorization':t}
     r = requests.get(BASE_URL+'/user',headers=headers)
+    r = requests.get(BASE_URL+'/transactions',headers=headers)
 
