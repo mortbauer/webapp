6f6bd1d1994bc6d0bc5608d9671cb2daeefa337a
Martin Ortbauer <mortbauer@gmail.com>
2016-06-21 22:51:29 +0200

    use vanilla alembic without any sugar from flask-migrate

--- migrations/env.py
+++ migrations/env.py
@@ -1,4 +1,5 @@
 from __future__ import with_statement
+import sys
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
@@ -15,14 +16,17 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from flask import current_app
-config.set_main_option('sqlalchemy.url', current_app.config.get('SQLALCHEMY_DATABASE_URI'))
-target_metadata = current_app.extensions['migrate'].db.metadata
+sys.path.insert(0,'.')
+import app
+from app import models
+target_metadata = models.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+config.set_main_option("sqlalchemy.url",app.config.DATABASE_URI)
+
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
@@ -37,10 +41,12 @@ def run_migrations_offline():
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url)
+    context.configure(
+        url=url, target_metadata=target_metadata, literal_binds=True)
 
     with context.begin_transaction():
         context.run_migrations()
+
 
 def run_migrations_online():
     """Run migrations in 'online' mode.
@@ -49,25 +55,21 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
-    engine = engine_from_config(
-                config.get_section(config.config_ini_section),
-                prefix='sqlalchemy.',
-                poolclass=pool.NullPool)
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool)
 
-    connection = engine.connect()
-    context.configure(
-                connection=connection,
-                target_metadata=target_metadata
-                )
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata
+        )
 
-    try:
         with context.begin_transaction():
             context.run_migrations()
-    finally:
-        connection.close()
 
 if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
-
---
+++ migrations/versions/3ec32628bdc6_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 3ec32628bdc6
+Revises: 
+Create Date: 2016-06-21 22:45:57.525003
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3ec32628bdc6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
