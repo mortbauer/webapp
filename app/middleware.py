0e85ed8aa8080535eb8be39aca4e3db195ffee97
Martin Ortbauer <mortbauer@gmail.com>
2016-06-29 00:32:58 +0200

    block everybody, if can't proof better

--- app/middleware.py
+++ app/middleware.py
@@ -4,7 +4,7 @@ async def endpoint_protection(app, handler):
     async def middleware_handler(request):
         if request.path not in app['acls']:
             needed = 'admin'
-        if request.headers['Authorization'] != 'admin':
+        if 'Authorization' not in request.headers or request.headers['Authorization'] != 'admin':
             return web.HTTPForbidden()
         return await handler(request)
     return middleware_handler
