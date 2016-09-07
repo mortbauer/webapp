9b8c38a320c815f97cf413ce81bf80111256d021
Martin Ortbauer <mortbauer@gmail.com>
2016-09-07 20:20:28 +0200

    added build command to manage.py

--- manage.py
+++ manage.py
@@ -12,15 +12,20 @@ def run():
 
 @run.command()
 @click.option('-g','--with-gunicorn',is_flag=True)
-def runserver(with_gunicorn):
+@click.option('--production',is_flag=True)
+def runserver(with_gunicorn,production):
     if not with_gunicorn:
         logging.basicConfig(level=logging.DEBUG)
         import asyncio
         from aiohttp import web
         web.run_app(app,port=5000)
     else:
+        if production:
+            address = 'unix:my_sock.sock'
+        else:
+            address = '0.0.0.0:5000'
         subprocess.call([
-            'gunicorn','-b','0.0.0.0:5000','-k','aiohttp.worker.GunicornWebWorker',
+            'gunicorn','-b',address,'-k','aiohttp.worker.GunicornWebWorker',
             '--reload','-w','1','-t','60','app.app:app','--log-level','DEBUG',
         ])
 
@@ -45,6 +50,19 @@ def create_example_data():
                 data['date'] = datetime.fromtimestamp(data['date'])
                 transactions.append(data)
             conn.execute(ins,transactions)
+
+@run.command()
+def build():
+    constants = []
+    with open('constants.txt','r') as f:
+        for line in f:
+            constants.append(line.strip())
+    with open('app/constants.py','w') as f:
+        for constant in constants:
+            f.write('{0} = "{0}"\n'.format(constant))
+    with open('static/src/constants/index.js','w') as f:
+        for constant in constants:
+            f.write('export const {0} = "{0}";\n'.format(constant))
 
 if __name__ == '__main__':
     run()
