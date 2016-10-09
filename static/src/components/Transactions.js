bc5258d57fdbbe9d24643648f7f43b6ca80b2fb1
Martin Ortbauer <mortbauer@gmail.com>
2016-10-09 22:26:23 +0200

    use own debounced Input fields

--- static/src/components/Transactions.js
+++ static/src/components/Transactions.js
@@ -6,6 +6,7 @@ import TextField from 'material-ui/TextField';
 import Transaction from './Transaction';
 import { getVisibleTransactions } from '../selectors';
 import Infinite from 'react-infinite';
+import DebouncedInput from './DebouncedInput';
 
 function mapStateToProps(state) {
     return {
@@ -43,34 +44,47 @@ export default class TransactionView extends React.Component {
     }
 
     renderTransactions(){
-        return this.props.transactions.map(t => <Transaction key={t.get('id')} onChange={(e)=>this.editIdValue(t.get('id'),e)} data={t}/>)
+        return this.props.transactions.map(
+            t => <Transaction key={t.get('id')} onChange={(e)=>this.editIdValue(t.get('id'),e)} data={t}/>)
     }
 
     render() {
         return (
                 <div>
                     <h1>Transactions</h1>
-                    <TextField
-                        id="filter_comment"
-                        hintText="Filter Comment"
-                        floatingLabelText="Filter Comment"
-                        onChange={(e) =>this.props.setFilter('comment',e.target.value)}
-                        defaultValue={this.props.filter.comment}
-                    />
-                    <TextField
-                        id="filter_amount"
-                        hintText="Filter Amount"
-                        floatingLabelText="Filter Amount"
-                        onChange={(e) =>this.props.setFilter('amount',e.target.value)}
-                        defaultValue={this.props.filter.amount}
-                    />
-                    <TextField
-                        id="filter_date"
-                        hintText="Filter Date"
-                        floatingLabelText="Filter Date"
-                        onChange={(e) =>this.props.setFilter('date',e.target.value)}
-                        defaultValue={this.props.filter.date}
-                    />
+                    <div style={{display:'flex', flexDirection:'row'}}>
+                        <div>
+                            <label style={{fontSize:'10'}}>Filter Comment</label>
+                            <DebouncedInput
+                                id="filter_comment"
+                                placeholder="Filter Comment"
+                                onChange={(value) =>this.props.setFilter('comment',value)}
+                                value={this.props.filter.get('comment')}
+                            />
+                        </div>
+                        <div>
+                            <label style={{fontSize:'10'}}>Filter Amount</label>
+                            <DebouncedInput
+                                id="filter_amount"
+                                placeholder="Filter Amount"
+                                hintText="Filter Amount"
+                                floatingLabelText="Filter Amount"
+                                onChange={(value) =>this.props.setFilter('amount',value)}
+                                value={this.props.filter.get('amount')}
+                            />
+                        </div>
+                        <div>
+                            <label style={{fontSize:'10'}}>Filter Date</label>
+                            <DebouncedInput
+                                id="filter_date"
+                                placeholder="Filter Date"
+                                hintText="Filter Date"
+                                floatingLabelText="Filter Date"
+                                onChange={(value) =>this.props.setFilter('date',value)}
+                                value={this.props.filter.get('date')}
+                            />
+                        </div>
+                    </div>
                     <Infinite containerHeight={800} elementHeight={20}>
                         {this.renderTransactions()}
                     </Infinite>
