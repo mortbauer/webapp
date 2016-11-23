b215e34e002ed0729c9b6b7b8d4ef4c9e9976777
Martin Ortbauer <mortbauer@gmail.com>
2016-11-23 22:32:49 +0100

    clean up a bit in transactions blueprint :wip:

--- static/src/transactions/actionTypes.js
+++ static/src/transactions/actionTypes.js
@@ -1,10 +1,4 @@
 export const GET = "transactions/GET";
-export const GET_SUCCESS = "transactions/GET_SUCCESS";
-export const GET_FAIL = "transactions/GET_FAIL";
 export const PUT = "transactions/PUT";
-export const PUT_SUCCESS = "transactions/PUT_SUCCESS";
-export const PUT_FAIL = "transactions/PUT_FAIL";
 export const PATCH = "transactions/PATCH";
-export const PATCH_SUCCESS = "transactions/PATCH_SUCCESS";
-export const PATCH_FAIL = "transactions/PATCH_FAIL";
 export const FILTER_SET = "transactions/FILTER_SET";
--- static/src/transactions/reducer.js
+++ static/src/transactions/reducer.js
@@ -2,34 +2,24 @@ import Immutable from 'immutable';
 
 import {
     GET,
-    GET_SUCCESS,
-    GET_FAIL,
     PUT,
     PATCH,
     FILTER_SET,
 } from './actionTypes';
 
 const initialState = Immutable.fromJS({
-    data: [],
+    data: {},
     filter: {
         comment: '',
         date: null,
         amount: null,
     },
+    subscribed: false,
     isFetching: false,
 });
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
-        case GET_SUCCESS:
-            return state.merge({
-                data: action.payload.data,
-                isFetching: false,
-            })
-        case GET_FAIL:
-            return state.merge({
-                isFetching: false,
-            })
         case GET:
             return state.merge({
                 isFetching: true,
